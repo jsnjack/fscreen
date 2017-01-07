3d03b78f5d64f3123d3ad6374f94d615af2ec123
Yauhen Shulitski <jsnjack@gmail.com>
2017-01-07 23:17:46 +0100

    Set gsettings

---
+++ fscreen.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+import argparse
+from subprocess import call
+
+
+class Message:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def main(args):
+    """
+    Adjust system configuration that scales system font
+    """
+    print("Applying font scale factor: %s" % args.factor)
+
+    code = call(["gsettings", "set", "org.gnome.desktop.interface", "text-scaling-factor", "%s" % args.factor])
+    print_result("Applying GNOME settings", code)
+
+
+def print_result(message, code):
+    if code == 0:
+        color = Message.GREEN
+        result = "OK"
+    else:
+        color = Message.FAIL
+        result = "ERROR(%s)" % code
+
+    print("    " + color + message + "... " + result)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("factor",
+                        type=float,
+                        action='store',
+                        help='Font scale factor')
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
